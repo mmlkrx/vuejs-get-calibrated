5f3d5d37c88c6f7ef95cbef4c75be8b7dbb3637a
Matthias Loker <mmloker@gmail.com>
2016-01-11 22:33:17 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="jade">
+  .container
+    <introduction></introduction>
+</template>
+
+<script>
+import Introduction from './components/Introduction.vue'
+
+export default {
+  components: {
+    Introduction
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-family: Helvetica, sans-serif;
+}
+
+.container {
+  width: 50em;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Introduction.vue
@@ -0,0 +1,15 @@
+<template lang="jade">
+  .introduction
+    h1 Time to get calibrated
+    p {{ msg }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'This website will help you assess your 90% confidence interval. This means that if you make 10 statements and you\'re 90% sure each individual statement is true, you should be right 9 out of 10 times.'
+    }
+  }
+}
+</script>
