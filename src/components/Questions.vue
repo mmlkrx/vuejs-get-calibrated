59c97a3dbdf5d45ca11d638a7ae2491fc83fda63
Matthias Loker <mmloker@gmail.com>
2016-01-13 23:55:45 +0100

    Compute confidence level

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -4,11 +4,11 @@
     ul
       li.question(v-for="question in questions")
         .text {{ question.text }}
-        form
-          input.lower(type="text")
+        .inputs
+          input.lower(type="text" v-model="question.lower")
           span to
-          input.upper(type="text")
-          input(type="submit")
+          input.upper(type="text" v-model="question.upper")
+          span Your current confidence level {{ confidence }}0%
 </template>
 
 <script>
@@ -16,17 +16,29 @@ export default {
   data () {
     return {
       questions: [
-        { text: 'In 1938 a British steam locomotive set a new speed record by going how fast? (km/h)', correctAnwser: 126 },
-        { text: 'In what year did Newton publish the universal laws of gravitation?', correctAnwser: 1685 },
-        { text: 'The Arpanet was established as a military communications system in what year?', correctAnwser: 1969 },
-        { text: 'How many centimeter is a typical business card long?', correctAnwser: 8.89 },
-        { text: 'What year was William Shakespeare born?', correctAnwser: 1564 },
-        { text: 'What is the air distance between New York and Los Angeles in kilometer?', correctAnwser: 3945 },
-        { text: 'What percentage of a square could be covered by a circle of the same width?', correctAnwser: 78.5 },
-        { text: 'How old was Charlie Chaplin when he died?', correctAnwser: 88 },
-        { text: 'How many pounds does a 350 page book weigh?', correctAnwser: 1.23 },
-        { text: 'The TV show Gilligan\'s Island first aired in which year?', correctAnwser: 1964 }
+        { text: 'In 1938 a British steam locomotive set a new speed record by going how fast? (km/h)', correctAnwser: 126, lower: '', upper: '' },
+        { text: 'In what year did Newton publish the universal laws of gravitation?', correctAnwser: 1685, lower: '', upper: '' },
+        { text: 'The Arpanet was established as a military communications system in what year?', correctAnwser: 1969, lower: '', upper: '' },
+        { text: 'How many centimeter is a typical business card long?', correctAnwser: 8.89, lower: '', upper: '' },
+        { text: 'What year was William Shakespeare born?', correctAnwser: 1564, lower: '', upper: '' },
+        { text: 'What is the air distance between New York and Los Angeles in kilometer?', correctAnwser: 3945, lower: '', upper: '' },
+        { text: 'What percentage of a square could be covered by a circle of the same width?', correctAnwser: 78.5, lower: '', upper: '' },
+        { text: 'How old was Charlie Chaplin when he died?', correctAnwser: 88, lower: '', upper: '' },
+        { text: 'How many pounds does a 350 page book weigh?', correctAnwser: 1.23, lower: '', upper: '' },
+        { text: 'The TV show Gilligan\'s Island first aired in which year?', correctAnwser: 1964, lower: '', upper: '' }
       ]
+    }
+  },
+
+  computed: {
+    confidence () {
+      var level = 0;
+      this.questions.forEach(function(question) {
+        if (question.correctAnwser > question.lower && question.correctAnwser < question.upper) {
+          level++;
+        }
+      });
+      return level;
     }
   }
 }
@@ -37,7 +49,7 @@ export default {
     list-style-type: none;
     padding: 0;
 
-    form {
+    .inputs {
       margin: 1em 0 2.5em 0;
 
       input { vertical-align: top; }
@@ -59,20 +71,6 @@ export default {
 
       .upper {
         margin: 0 1em;
-      }
-
-      input[type="submit"] {
-        padding: .6em 1em;
-        border: 0;
-        background-color: #4BC74B;
-        color: white;
-        font-size: 1em;
-        border-radius: 2px;
-
-        &:hover {
-          background-color: darken(#4BC74B, 10%);
-          cursor: pointer;
-        }
       }
     }
   }
